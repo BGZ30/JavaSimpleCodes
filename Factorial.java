3b803315f7ae8e2a17e45f2abc987a67e3a61035
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-02-12 14:04:59 +0200

    All updated

---
+++ Factorial.java
@@ -0,0 +1,53 @@
+
+/*
+    Calculating the Factorial.
+
+    NOTE: to get (n!), n must be geater than or equal zero.
+
+    Implemented using two methodes:
+        - With recursion
+        - Without recursion
+*/
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n;
+        long facto;
+
+        Scanner s = new Scanner(System.in);
+
+        do {
+            System.out.print("Please enter a positive integer: ");
+            n = s.nextInt();
+        } while (n < 0);
+
+        // Calculating the factorial without recursion
+        facto = findFacto.NoRec(n);
+        System.out.println("Without Recursion, " + n + "! = " + facto);
+
+        // Calculating the factorial with recursion
+        facto = findFacto.Rec(n);
+        System.out.println("With Recursion, " + n + "! = " + facto);
+    }
+}
+
+class findFacto {
+    static long NoRec(int n) {
+        long facto = 1;
+
+        while (n != 0) {
+            facto *= n;
+            n--;
+        }
+
+        return facto;
+    }
+
+    static long Rec(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return (n * Rec(n - 1));
+    }
+}
