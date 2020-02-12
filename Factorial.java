01d32ab0289ebaa8f9c0c1a965a1e55e4ca0261f
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-02-12 13:03:59 +0200

    implemented the recursive method

--- Factorial.java
+++ Factorial.java
@@ -36,7 +36,7 @@ class findFacto {
 
         while (n != 0) {
             facto *= n;
-            n -= 1;
+            n--;
         }
 
         return facto;
@@ -47,6 +47,5 @@ class findFacto {
             return 1;
         else
             return (n * Rec(n - 1));
-
     }
 }
